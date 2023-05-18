f52617bfbe8e6ec5037a90d79df6dde9d0aae2fa
Roman Boytsov <domovon@outlook.com>
2023-05-18 13:38:10 +0300

    Main.java changed

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int initial_account = 150;
+        int deposit = 115_000;
+        int bonus = 1;
+        int rubles_for_bonus = 100;
+        int total_bonus;
+        int final_account;
+
+        if (deposit > rubles_for_bonus) {
+            total_bonus = deposit / rubles_for_bonus * bonus;
+        } else {
+            total_bonus = 0;
+        }
+        final_account = initial_account + deposit + total_bonus;
+        System.out.printf("Итоговый счет: " + final_account +
+                ". Итоговый бонус: " + total_bonus);
     }
 }

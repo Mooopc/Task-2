dab0abe64746ecc8df32994ac835af9c78cdc927
Roman Boytsov <domovon@outlook.com>
2023-05-22 20:47:50 +0300

    CamelCasing

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        int initial_account = 150;
+        int initialAccount = 150;
         int deposit = 115_000;
         int bonus = 1;
-        int rubles_for_bonus = 100;
-        int total_bonus;
-        int final_account;
+        int rublesForBonus = 100;
+        int totalBonus;
+        int finalAccount;
 
-        if (deposit > rubles_for_bonus) {
-            total_bonus = deposit / rubles_for_bonus * bonus;
+        if (deposit > rublesForBonus) {
+            totalBonus = deposit / rublesForBonus * bonus;
         } else {
-            total_bonus = 0;
+            totalBonus = 0;
         }
-        final_account = initial_account + deposit + total_bonus;
-        System.out.printf("Итоговый счет: " + final_account +
-                ". Итоговый бонус: " + total_bonus);
+        finalAccount = initialAccount + deposit + totalBonus;
+        System.out.printf("Итоговый счет: " + finalAccount + ". Итоговый бонус: " + totalBonus);
     }
 }
